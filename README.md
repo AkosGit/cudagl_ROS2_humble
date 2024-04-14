e2174cd7c9f0f010c430687a6dfa4f195da189c8
Pálosi Ákos <46137184+AkosGit@users.noreply.github.com>
2024-04-14 14:07:47 +0200

    movement and turning left to lidar data implemented

--- README.md
+++ README.md
@@ -1,27 +1,4 @@
-# atinfinity/cudagl
-
-## Introduction
-
-This is a Dockerfile to use OpenGL/CUDA/Vulkan with ROS2 humble on Docker container with NVIDIA GPU.
-
-|glxgears|vulkaninfo|
-|---|---|
-|![glxgears](img/glxgears.png)|![vulkaninfo](img/vulkaninfo.png)|
-
-## Requirements
-
-- NVIDIA graphics driver
-- Docker
-- xorg-xhost
-- nvidia-container-toolkit
-
-## Build docker image
-
-### non-root user
-
-```bash
-docker build --build-arg UID=$(id -u) --build-arg GID=$(id -g) -t atinfinity/cudagl:11.8.0-cudnn8-devel-ubuntu22.04 .
-```
+## Container related
 
 ### root user
 
@@ -32,5 +9,29 @@ docker build -t atinfinity/cudagl:11.8.0-cudnn8-devel-ubuntu22.04 -f Dockerfile.
 ## Launch docker container
 
 ```bash
-./launch_container.sh
+sudo ./launch_container.sh
 ```
+### launch container terminal
+```bash
+sudo ./launch_terminal.sh
+```
+
+
+## Start project
+```bash
+cd ./workspace
+ulimit -n 1024
+gz sim building_robot.sdf
+ros2 run ros_gz_bridge parameter_bridge /lidar@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan
+ros2 run ros_gz_bridge parameter_bridge /cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist
+ros2 run ros_gz_bridge parameter_bridge /imu@sensor_msgs/msg/Imu@gz.msgs.IMU
+source ./ros2_ws/install/setup.bash
+colcon build --symlink-install
+ros2 run ros2_course talker
+```
+
+## Usefull
+- [ROS GZ bridge communication](https://github.com/gazebosim/ros_gz/tree/ros2/ros_gz_bridge)
+- [Harmonic tutorials](https://gazebosim.org/docs/harmonic/moving_robot)
+
+2.6-3.2
--- workspace/ros2_ws/src/ros2_course/ros2_course/talker.py
+++ workspace/ros2_ws/src/ros2_course/ros2_course/talker.py
@@ -1,29 +1,101 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String, Int32
-
+from sensor_msgs.msg import LaserScan, Imu
+from geometry_msgs.msg import Twist
+import math
 
 class MinimalSubscriber(Node):
 
     def __init__(self):
         super().__init__('minimal_subscriber')
-        self.subscription = self.create_subscription(
-            Int32,
-            '/keyboard/keypress',
-            self.listener_callback,
+        self.publisher = self.create_publisher(Twist, 'cmd_vel', 10)
+
+
+        self.lidar_subscription = self.create_subscription(
+            LaserScan,
+            '/lidar',
+            self.lidar_callback,
             1)
-        self.subscription  # prevent unused variable warning
+        self.lidar_subscription  # prevent unused variable warning
+        self.imu_subscription = self.create_subscription(
+            Imu,
+            '/imu',
+            self.imu_callback,
+            1)
+        self.imu_subscription  # prevent unused variable warning
 
-    def listener_callback(self, msg):
-        self.get_logger().info('I heard msg: "%s"' % msg.data)
 
+        self.turn_in_progress = False
+        self.target_angle = math.pi / 2
+        
+        print("Sending start instruction...")
+        self.forward(0.3)
+
+    def imu_callback(self, msg):
+        orientation = msg.orientation
+        # Convert quaternion to roll, pitch, yaw (Euler angles)
+        roll, pitch, yaw = self.quaternion_to_euler(orientation.x, orientation.y, orientation.z, orientation.w)
+        self.roll = roll
+        self.pitch = pitch
+        self.yaw = yaw
+
+    def lidar_callback(self, msg):
+        min_dist = min(msg.ranges)
+        #self.get_logger().info('min: "%s"' % min_dist)
+        if not self.turn_in_progress:
+            if min_dist < 3.0:
+                self.turn_left(0.05)
+        else:
+            if abs(self.yaw) >= abs(self.target_angle)-0.17:
+                print(self.yaw)
+                self.forward(0.3)
+                self.turn_in_progress = False
+
+    def turn_left(self, speed):
+        vel_msg = Twist()
+        vel_msg.linear.x = speed
+        vel_msg.linear.y = 0.0
+        vel_msg.linear.z = 0.0
+        vel_msg.angular.x = 0.0
+        vel_msg.angular.y = 0.0
+        vel_msg.angular.z = math.pi / 10
+        self.publisher.publish(vel_msg)
+        self.turn_in_progress = True
+        self.target_angle = math.pi / 2
+
+    def forward(self, speed):
+        vel_msg = Twist()
+        vel_msg.linear.x = speed
+        vel_msg.linear.y = 0.0
+        vel_msg.linear.z = 0.0
+        vel_msg.angular.x = 0.0
+        vel_msg.angular.y = 0.0
+        vel_msg.angular.z = 0.0
+        self.publisher.publish(vel_msg)
+
+    def quaternion_to_euler(self, x, y, z, w):
+        # Convert quaternion to roll, pitch, yaw (Euler angles)
+        t0 = +2.0 * (w * x + y * z)
+        t1 = +1.0 - 2.0 * (x * x + y * y)
+        roll = math.atan2(t0, t1)
+
+        t2 = +2.0 * (w * y - z * x)
+        t2 = +1.0 if t2 > +1.0 else t2
+        t2 = -1.0 if t2 < -1.0 else t2
+        pitch = math.asin(t2)
+
+        t3 = +2.0 * (w * z + x * y)
+        t4 = +1.0 - 2.0 * (y * y + z * z)
+        yaw = math.atan2(t3, t4)
+
+        return roll, pitch, yaw
 
 def main(args=None):
-    print("test")
+    print("Node started...")
     rclpy.init(args=args)
     minimal_subscriber = MinimalSubscriber()
     rclpy.spin(minimal_subscriber)
-    print("test")
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
