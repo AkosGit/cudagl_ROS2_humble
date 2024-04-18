b5a50fa30bae052089dc9ee4ae880ec7de0c65ee
Pálosi Ákos <46137184+AkosGit@users.noreply.github.com>
2024-04-18 23:11:45 +0200

    turning only works when its not shifiting forward

--- workspace/ros2_ws/src/ros2_course/ros2_course/talker.py
+++ workspace/ros2_ws/src/ros2_course/ros2_course/talker.py
@@ -14,6 +14,11 @@ class MinimalSubscriber(Node):
         self.pitch = 0
         self.yaw = 0
 
+        # 0: forward, 1:left, 2:right
+        self.direction = 0
+        self.current_distance = 0
+        self.prev_distance = 0
+
         self.lidar_subscription = self.create_subscription(
             LaserScan,
             '/lidar',
@@ -30,7 +35,7 @@ class MinimalSubscriber(Node):
 
         self.turn_in_progress = False
         self.target_angle = math.pi / 2
-        
+
         print("Sending start instruction...")
         self.forward(0.3)
 
@@ -41,18 +46,44 @@ class MinimalSubscriber(Node):
         self.roll = roll
         self.pitch = pitch
         self.yaw = yaw
+        self.get_logger().info('"%s"' % yaw)
 
     def lidar_callback(self, msg):
-        min_dist = min(msg.ranges)
+        self.current_distance = min(msg.ranges)
         #self.get_logger().info('min: "%s"' % min_dist)
         if not self.turn_in_progress:
-            if min_dist < 3.0:
+            if self.current_distance < 2.0:
                 self.turn_left(0.05)
+        # turn in progress
         else:
-            if abs(self.yaw) >= abs(self.target_angle)-0.18:
-                print(self.yaw)
+            # when its reached angle
+            if abs(self.yaw) >= abs(self.target_angle)-0.1 or abs(self.yaw) >= abs(self.target_angle)+0.1:
+                self.get_logger().info('target angle reached: "%s"' % self.target_angle)
                 self.forward(0.3)
                 self.turn_in_progress = False
+                self.direction = 0
+            else:
+                # when robot is stuck, it should have moved more than 0.1
+                if self.prev_distance+0.1 > self.current_distance and self.prev_distance-0.1 < self.current_distance:
+                    self.get_logger().info('stuck: "%s"' % self.current_distance)
+                    if self.direction == 2:
+                        # reset to forward, should not be reached
+                        self.direction = 0
+                    else:
+                        self.direction = self.direction + 1
+
+        self.current_distance = self.prev_distance
+
+    def forward(self, speed):
+        vel_msg = Twist()
+        vel_msg.linear.x = speed
+        vel_msg.linear.y = 0.0
+        vel_msg.linear.z = 0.0
+        vel_msg.angular.x = 0.0
+        vel_msg.angular.y = 0.0
+        vel_msg.angular.z = 0.0
+        self.publisher.publish(vel_msg)
+        self.direction = 0
 
     def turn_left(self, speed):
         vel_msg = Twist()
@@ -64,7 +95,14 @@ class MinimalSubscriber(Node):
         vel_msg.angular.z = math.pi / 11
         self.publisher.publish(vel_msg)
         self.turn_in_progress = True
-        self.target_angle = math.pi / 2
+        target = self.yaw + math.pi / 2
+        if(target > math.pi):
+            target = math.pi - (target - math.pi)
+        self.get_logger().info('target angle:"%s"' % target)
+        self.get_logger().info('Current angle:"%s"' % self.yaw)
+        self.target_angle = target
+        self.direction  = 1
+        self.get_logger().info('turn left in progress')
 
     def turn_right(self, speed):
         vel_msg = Twist()
@@ -76,17 +114,14 @@ class MinimalSubscriber(Node):
         vel_msg.angular.z = - math.pi / 11
         self.publisher.publish(vel_msg)
         self.turn_in_progress = True
-        self.target_angle = - math.pi / 2
-
-    def forward(self, speed):
-        vel_msg = Twist()
-        vel_msg.linear.x = speed
-        vel_msg.linear.y = 0.0
-        vel_msg.linear.z = 0.0
-        vel_msg.angular.x = 0.0
-        vel_msg.angular.y = 0.0
-        vel_msg.angular.z = 0.0
-        self.publisher.publish(vel_msg)
+        target = self.yaw - math.pi / 2
+        if(target <  0):
+            target = 0 - (0 - target)
+        self.get_logger().info('target angle:"%s"' % target)
+        self.get_logger().info('Current angle:"%s"' % self.yaw)
+        self.target_angle = target
+        self.direction = 2
+        self.get_logger().info('turn left in progress')
 
     def quaternion_to_euler(self, x, y, z, w):
         # Convert quaternion to roll, pitch, yaw (Euler angles)
